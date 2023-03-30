472b11360da1fefc2e45fd660563bfbb62e6b187
VadimTolstov <tolstov50@mail.ru>
2023-03-30 14:44:56 +0300

    1

--- src/test/java/hw_13_Jenkins_Property/TestBaseFaker.java
+++ src/test/java/hw_13_Jenkins_Property/TestBaseFaker.java
@@ -3,7 +3,6 @@ package hw_13_Jenkins_Property;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import hw_13_Jenkins_Property.helpers.Attach;
-import io.qameta.allure.Step;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -15,14 +14,15 @@ import java.util.Map;
 public class TestBaseFaker {
 
     @BeforeAll
-    @Step("chrome v 100.0, 1920x1080")
+
     static void beforeAll() {
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browser = "chrome";
-        Configuration.browserVersion = "100.0";
-        Configuration.browserSize = "1920x1080";
-        //подлючаем
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@" + System.getProperty("selenoid_url", "selenoid.autotests.cloud/wd/hub");
+        Configuration.baseUrl = System.getProperty("base_url", "https://demoqa.com");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browser_version", "100.0");
+        Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
+
+
         //Включаем VNC окошка в окошке и  включаем запись видео Video
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
@@ -31,10 +31,12 @@ public class TestBaseFaker {
         ));
         Configuration.browserCapabilities = capabilities;
     }
+
     @BeforeEach
     void addListener() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
+
     @AfterEach
     void addAttachments() {
         //скриншот
@@ -44,42 +46,6 @@ public class TestBaseFaker {
         //логи
         Attach.browserConsoleLogs();
         //видео
-        Attach.addVideo();
-    }
-}
-    @BeforeAll
-    public static void setup() {
-        SelenideLogger.addListener("allure", new AllureSelenide());
-
-        Configuration.remote = "https://user1:1234@" + System.getProperty("selenoid_url", "selenoid.autotests.cloud/wd/hub");
-
-
-        Configuration.baseUrl = System.getProperty("base_url", "https://demoqa.com" );
-
-
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browser_version", "100.0");
-        Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
-
-        Configuration.browserCapabilities = capabilities;
-    }
-
-    @BeforeEach
-    void addListener(){
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-    }
-
-    @AfterEach
-    void addAttachments() {
-        Attach.screenshotAs("Last screenshot");
-        Attach.pageSource();
-        Attach.browserConsoleLogs();
         Attach.addVideo();
     }
 
