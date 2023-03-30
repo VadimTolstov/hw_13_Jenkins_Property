b42b6a0e12e4e2bd71f4557ef844b5e2d6890dbf
VadimTolstov <tolstov50@mail.ru>
2023-03-30 13:29:03 +0300

    1

---
+++ src/test/java/hw_13_Jenkins_Property/RegistrationWithPageObjectsFakerTests.java
@@ -0,0 +1,58 @@
+package hw_13_Jenkins_Property;
+
+
+import hw_13_Jenkins_Property.pages.RegistrationPageFaker;
+import hw_13_Jenkins_Property.pages.components.RegistrationResultsModal;
+import hw_13_Jenkins_Property.utils.DataGenerator;
+import hw_13_Jenkins_Property.utils.StudentData;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Регистрация студента")
+@Tag("simple")
+public class RegistrationWithPageObjectsFakerTests extends TestBaseFaker {
+    RegistrationPageFaker registrationPage = new RegistrationPageFaker();
+    StudentData studentData = DataGenerator.getRandomStudent();
+    RegistrationResultsModal registrationResultsModal = new RegistrationResultsModal();
+
+    @Test
+    @Regress
+    @DisplayName("Регистрация студента")
+    void fillFormTest() {
+
+        registrationPage.openPage()
+                .removeBanner()
+                .removeFooter()
+                .setFirstName(studentData.getFirstName())
+                .setLastName(studentData.getLastName())
+                .setUserEmail(studentData.getUserEmail())
+                .setGender(studentData.getGender())
+                .setUserPhone(studentData.getUserPhone())
+                .setBirthData(studentData.getDay(),
+                        studentData.getMonth(),
+                        studentData.getYear())
+                .setSubjects(studentData.getSubject())
+                .setHobby(studentData.getHobby())
+                .setUploadImage(studentData.getFilePath())
+                .setAddress(studentData.getAddress())
+                .setState(studentData.getState())
+                .setCity(studentData.getCity())
+                .registerForm();
+
+
+        registrationResultsModal.verifyModalAppears()
+                .verifyResult("Student Name", studentData.getFirstName() + " " + studentData.getLastName())
+                .verifyResult("Student Email", studentData.getUserEmail())
+                .verifyResult("Gender", studentData.getGender())
+                .verifyResult("Mobile", studentData.getUserPhone())
+                .verifyResult("Date of Birth", studentData.getDay() + " " + studentData.getMonth() + "," + studentData.getYear())
+                .verifyResult("Subjects", studentData.getSubject())
+                .verifyResult("Hobbies", studentData.getHobby())
+                .verifyResult("Picture", studentData.getFilePath()
+                        .substring(studentData.getFilePath().lastIndexOf("/") + 1))
+                .verifyResult("Address", studentData.getAddress())
+                .verifyResult("State and City", studentData.getState() + " " + studentData.getCity());
+
+    }
+}
---
+++ src/test/java/hw_13_Jenkins_Property/TestBaseFaker.java
@@ -0,0 +1,86 @@
+package hw_13_Jenkins_Property;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import hw_13_Jenkins_Property.helpers.Attach;
+import io.qameta.allure.Step;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+public class TestBaseFaker {
+
+    @BeforeAll
+    @Step("chrome v 100.0, 1920x1080")
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browser = "chrome";
+        Configuration.browserVersion = "100.0";
+        Configuration.browserSize = "1920x1080";
+        //подлючаем
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        //Включаем VNC окошка в окошке и  включаем запись видео Video
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+    }
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+    @AfterEach
+    void addAttachments() {
+        //скриншот
+        Attach.screenshotAs("Last screenshot");
+        //дерево
+        Attach.pageSource();
+        //логи
+        Attach.browserConsoleLogs();
+        //видео
+        Attach.addVideo();
+    }
+}
+    @BeforeAll
+    public static void setup() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        Configuration.remote = "https://user1:1234@" + System.getProperty("selenoid_url", "selenoid.autotests.cloud/wd/hub");
+
+
+        Configuration.baseUrl = System.getProperty("base_url", "https://demoqa.com" );
+
+
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browser_version", "100.0");
+        Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    void addListener(){
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
+
+}
